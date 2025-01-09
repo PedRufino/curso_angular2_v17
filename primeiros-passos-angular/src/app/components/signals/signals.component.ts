4139535d1bed27d0d05589553391c8105217d180
Pedro Rufino <pedro.rufino@nappsolutions.com>
2025-01-09 13:35:56 -0300

    [Feat]: Adicionando aula de signals

---
+++ primeiros-passos-angular/src/app/components/signals/signals.component.ts
@@ -0,0 +1,53 @@
+import { Component, computed, effect, signal } from '@angular/core';
+
+@Component({
+  selector: 'app-signals',
+  imports: [],
+  templateUrl: './signals.component.html',
+  styleUrl: './signals.component.scss'
+})
+export class SignalsComponent {
+  public firstName = signal('Pedro ');
+  public lastName = signal('Rufino');
+
+  public fullName = computed(() => {
+    return this.firstName() + this.lastName();
+  })
+
+  public array = signal([1]);
+
+  /*
+    effect - raramente são necessários na maioria dos códigos,
+    mas podem ser úteis em circunstâncias específicas.
+    - Registro de dados sendo exibidos e quando eles mudam, seja para análise
+      ou como ferramenta de depuração.
+
+    - Manter os dados sincronizados com o window.localStorage.
+
+    - Adicionando comportamento DOM personalizado que não pode ser expresso com
+      sintaxe de modelo.
+
+    - Executar renderização personalizada em um <canvas>, biblioteca de gráficos
+      ou outra biblioteca de UI de terceiros.
+  */
+
+  constructor() {
+    effect(() => {
+      console.log(this.firstName());
+    })
+  }
+
+  public updateName() {
+    return this.firstName.set("João ")
+  }
+
+  public updateArray() {
+    // this.firstName.update(() => {
+    //   return "João "
+    // })
+
+    this.array.update((oldValue: Array<number>) => {
+      return [...oldValue, oldValue.length + 1]
+    })
+  }
+}
