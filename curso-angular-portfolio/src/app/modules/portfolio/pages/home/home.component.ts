6eee06d2cff22bdf8b002c196dd8a2d938d6d355
Pedro Rufino <pedro.rufino@nappsolutions.com>
2025-01-09 14:39:27 -0300

    [Feat]: Header component

--- curso-angular-portfolio/src/app/modules/portfolio/pages/home/home.component.ts
+++ curso-angular-portfolio/src/app/modules/portfolio/pages/home/home.component.ts
@@ -1,8 +1,13 @@
 import { Component } from '@angular/core';
 
+// components
+import { HeaderComponent } from '../../components/header/header.component';
+
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [
+    HeaderComponent,
+  ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
