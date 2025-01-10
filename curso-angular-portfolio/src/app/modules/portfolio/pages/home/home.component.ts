85969a33362602f7ea45e41a6a345430b926661f
Pedro Rufino <pedro.rufino@nappsolutions.com>
2025-01-10 09:46:31 -0300

    [Feat]: Knowledge component

--- curso-angular-portfolio/src/app/modules/portfolio/pages/home/home.component.ts
+++ curso-angular-portfolio/src/app/modules/portfolio/pages/home/home.component.ts
@@ -2,11 +2,13 @@ import { Component } from '@angular/core';
 
 // components
 import { HeaderComponent } from '../../components/header/header.component';
+import { KnowledgeComponent } from '../../components/knowledge/knowledge.component';
 
 @Component({
   selector: 'app-home',
   imports: [
     HeaderComponent,
+    KnowledgeComponent,
   ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
